190762b9dda9a3891151031f04036cfdfb69efdb
Guisella2906 <163216917+Guisella2906@users.noreply.github.com>
2024-04-30 21:21:57 -0400

    Add files via upload

---
+++ img/script.js
@@ -0,0 +1,22 @@
+const gato = {
+    nombre: "Luna",
+    color : ['blanco', 'gris']
+   /* maullar: function(volumen){
+        return "miau miau en volumen ${volumen}"
+    }*/
+    jugueteFavorito : 'ratita de plastico',
+    jugarconjugueteFavorito : funcion(juguete){return "estoy jugando con mi: "}
+}
+
+console.log(gato.jugarconjugueteFavorito(gato.jugueteFavorito))
+
+
+/*
+console.log(
+    "El nombre del objeto gato es: "
+    + gato.nombre
+)
+
+console.log('El gato quiere decir :'
+${gato.maullar('despacio')}')
+)*/
